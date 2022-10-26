d74b10a0c4e7cb78f97509f2dea2fd1b753b6c02
cleon5 <cleon5@ucol.mx>
2022-10-26 08:43:21 -0500

    add funcionality Save

---
+++ src/Constants/Guarda.js
@@ -0,0 +1,42 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+//import React, { useState, useEffect } from "react";
+
+export const storeData = async (name, data) => {
+  try {
+    await AsyncStorage.setItem(name, data);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getData = async (name) => {
+    let value;
+    try {
+    value = await AsyncStorage.getItem(name);
+    if (value !== null) {
+      return value;
+    }
+  } catch (e) {
+    console.log(e + 'erro');
+  }
+}
+//Json saves fuctions
+export const storeDataJson = async (name, data) => {
+  try {
+    const jsonValue = JSON.stringify(data)
+    await AsyncStorage.setItem(name, jsonValue)
+    console.log(jsonValue + name)
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export const getDataJson = async (name) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(name)
+    console.log(jsonValue)
+    return jsonValue != null ? JSON.parse(jsonValue) : null
+  } catch(e) {
+    console.log(e);
+  }
+}
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -12,6 +12,7 @@ import { styleHome } from "../Constants/Styles";
 import { Entypo } from "@expo/vector-icons";
 import OpenLink from "../Components/OpenLink";
 import DoubleClick from "react-native-double-tap";
+import {getDataJson, storeDataJson} from "../Constants/Guarda";
 
 export default class Home extends Component {
   constructor(props) {
@@ -20,12 +21,48 @@ export default class Home extends Component {
       Comic: [],
       trans: "",
       More: true,
+      Guardar: false,
     };
   }
   componentDidMount() {
     this.get();
+    this.Cargar();
   }
 
+  async Guardar(name, data) {
+    let get = [];
+    let x = [];
+    x = await getDataJson("FavComic");
+    console.log(x);
+    if (x == null) {
+      await storeDataJson(name, data);
+    } else {
+      get.push(data);
+      if (Array.isArray(x)) {
+        if (x.includes(data)) {
+          get = x;
+          get.splice(x.indexOf(data), 1);
+          console.log(x.indexOf(data));
+        } else x.map((a) => get.push(a));
+      } else x != get ? get.push(x) : (get = null);
+      console.log(get);
+      await storeDataJson(name, get);
+    }
+    this.setState({ Guardar: !this.state.Guardar });
+  }
+
+  async Cargar() {
+    let x = await getDataJson("FavComic");
+    console.log(x)
+    if (Array.isArray(x))
+      x.includes(this.state.Comic.num)//Editar id
+        ? this.setState({ Guardar: true })
+        : this.setState({ Guardar: false });
+    else
+      x == this.state.Comic.num && x != null
+        ? this.setState({ Guardar: true })
+        : this.setState({ Guardar: false });
+  }
   async get() {
     let x = await getComic(2500);
     this.setState({ Comic: x });
@@ -45,25 +82,30 @@ export default class Home extends Component {
       });
     }
   }
+
   render() {
     return (
       <ScrollView>
-        <View style={styles.container}>
+        <View style={{flex:1}}>
           <DoubleClick
             singleTap={() => {
               console.log("single tap");
             }}
             doubleTap={() => {
+              this.Guardar("FavGames", this.state.Comic.num);
               console.log("double tap");
             }}
             delay={200}
           >
             <Text style={styleHome.titulo}>{this.state.Comic.title}</Text>
+            <Text style={styleHome.titulo}>{this.state.Guardar} "am"</Text>
+
             <Image
               style={styleHome.img}
               source={{ uri: this.state.Comic.img }}
             />
           </DoubleClick>
+
           <View>
             <Text style={styleHome.titulo}>Information</Text>
             <Text style={styleHome.alt}>{this.state.Comic.alt}</Text>
@@ -111,9 +153,3 @@ export default class Home extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
